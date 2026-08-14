2a8680727bb5a25d93404d7a1bd77cf4d8b3f3d1
jreynoso111 <jreynoso111@gmail.com>
2026-08-13 20:46:51 -0400

    Improve admin analytics dashboard

--- scripts/dev_server.py
+++ scripts/dev_server.py
@@ -84,7 +84,7 @@ class Handler(SimpleHTTPRequestHandler):
                     "default-src 'self'; base-uri 'self'; object-src 'none'; img-src 'self' data: blob: https:; "
                     "media-src 'self' data: blob: https:; script-src 'self' "
                     "'sha256-j6jze/KNzX7uxTlJ985Eb7uarz/gAZrqLrei3C3EUSI=' "
-                    "'sha256-gQn41OalzsjhX/svhbZ8DFr1sg2kau/EgpWBrxEepz8='; "
+                    "'sha256-E55ktwBxfSE0PATbK3U9nbBLFjleqJdN1pwDYz6i4Pk='; "
                     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com data:; "
                     "connect-src 'self' https:; form-action 'self'; frame-ancestors 'none'"
                 ),
